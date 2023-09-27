e05e2095274541b956eb581ee1932819754cdcc0
Angel Efrain Romero Zepeda <angel.romero@unealum.local>
2023-09-27 13:34:56 +0200

    prg1

---
+++ HolaMundo.java
@@ -0,0 +1,38 @@
+class HolaMundo {
+	public static void main(String [] args){
+	
+	System.out.print("Hola Mundo.");
+		System.out.println(" Hoooola");
+	System.out.println("Hace frio");
+	System.out.println("Adios mundo cruel");
+	
+		double precioVenta;
+		int miEdad = 18;
+		int miEdadEnMeses;
+		int
+		
+
+		precioVenta =  = 185;
+		miEdad = 48;
+		miEdadEnMeses = miEdad * 12;
+
+			System.out.println("El precio es " + precioVenta + " euros");
+			
+
+
+
+
+
+
+
+
+
+
+
+	System.out.println("Mi edad es: " + miEdad);
+	
+	String precioConSimbolo = precioVenta + "€";
+	System.out.println(precioConSimbolo);
+
+	}
+}
---
+++ Prints.java
@@ -0,0 +1,15 @@
+class HolaMundo {
+	public static void main(String [] args){
+	
+	System.out.print("Hola Mundo.");
+		System.out.println("Hoooola");
+	System.out.println("Hace frio");
+	System.out.println("Adios mundo cruel");
+	
+		double precioVenta = 25.4;
+		int miEdad = 18;
+	
+	System.out.println("El precio es " + precioVenta + "€");
+	System.out.println("Mi edad es: " + miEdad);
+	}
+}
